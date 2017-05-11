5a23c2405466eefd9ab080abe49f1b9d431a68c1
archana parmar <archana.parmar@letsnurture.com>
2017-05-11 11:08:46 +0530

    Initial Commit

---
+++ LNJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LNJenkins
+//
+//  Created by LN-MAC-05 on 11/05/17.
+//  Copyright © 2017 LN-MAC-05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
